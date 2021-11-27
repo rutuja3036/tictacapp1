a50ee07923fa3ce0c689efbd5f1c82cb026144b1
rutuja <rutujaingale0230@example.com>
2021-11-27 21:50:43 +0530

    final tictacapp

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import Square from './Square'
-export default class Board extends component {
-    renderSuare(i) {
+import React, { Component } from 'react';
+import Square from './Square';
+export default class Board extends Component {
+    renderSquare(i) {
         return<Square value = {this.props.squares[i]}
-        onClick={() => this.props.onClick[i]}
+        onClick={() => this.props.onClick(i)}
         />
     }
     render() {
